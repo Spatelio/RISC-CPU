c3e39d8ad2d4afa43388d5eae54c8a5f0b6355ef
Robert Bevilacqua <105384302+rob-bevilacqua@users.noreply.github.com>
2026-03-02 10:43:21 -0500

    removing unused alu params, well define them in phase3

--- src/alu.v
+++ src/alu.v
@@ -23,9 +23,7 @@ module alu(
     parameter INC    = 5'b10000;
 
     //non alu used outputs putting them here so we know theyre taken
-    parameter LD     = 5'b10001;
-    parameter LDI    = 5'b10010;
-
+    
     // Wires for functional unit output
     wire [31:0] add_result;
     wire [31:0] sub_result;
